f2aa1e83e7a52e188b908a3a479e9da94fbe81b7
alexanderminton001 <alexanderminton131@gmail.com>
2026-03-04 06:18:41 +0000

    Tue, Mar 3, 2026, 10:18 PM -08:00

---
+++ js/loading.js
@@ -0,0 +1,13 @@
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+
+
+fuction blurring() {
+    load++
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
