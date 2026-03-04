4a3eacdf8b0a373761d4abe10672159ae5e740d5
alexanderminton001 <alexanderminton131@gmail.com>
2026-03-04 06:25:50 +0000

    Tue, Mar 3, 2026, 10:25 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,11 +3,16 @@ bg = document.querySelector('.bg')
 
 load = 0
 
+int = setInterval(blurring, 30)
 
-
-fuction blurring() {
+function blurring() {
     load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
     loadText.innerText = `${load}%`
-    loadText.style.opacity = 1
-    bg.style.filter = "blur(10px)"
+    loadText.style.opacity = 1 - load/100
+
+    bg.style.filter = `blur(${30-load/100*30}px)`
 }
