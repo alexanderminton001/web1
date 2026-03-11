cc10e5a96018fa857042e4370e767079193e9ba2
alexanderminton001 <alexanderminton131@gmail.com>
2026-03-11 16:45:03 +0000

    Wed, Mar 11, 2026, 9:45 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -1,31 +1,16 @@
 buttons = document.querySelector('.buttons')
-applause = document.querySelector('.applause')
-boo = document.querySelector('.boo')
-gasp = document.querySelector('.gasp')
-tada = document.querySelector('.tada')
-victory = document.querySelector('.victory')
-wrong = document.querySelector('.wrong')
 
 sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
-
 
 for (let i = 0; i < 6; i = i + 1) {
     btn = document.createElement('button')
     btn.innerText = sounds[i]
     btn.classList.add('btn')
     btn.addEventListener('click', () => {
-        applause.play()
-})
-buttons.appendChild(btn)
+        document.querySelector(`.${sounds[i]}`).play()
+    })
+    buttons.appendChild(btn)
 }
-
-btn = document.createElement('button')
-btn.innerText = "applause"
-btn.classList.add('btn')
-btn.addEventListener('click', () => {
-    applause.play()
-})
-buttons.appendChild(btn)
 
 
 
