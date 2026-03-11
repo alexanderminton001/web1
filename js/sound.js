2963791e415341f12f175a8c84aca288ab9b77e7
alexanderminton001 <alexanderminton131@gmail.com>
2026-03-11 16:33:48 +0000

    Wed, Mar 11, 2026, 9:33 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -4,6 +4,6 @@ btn = document.createElement('button')
 btn.innerText = "applause"
 btn.classList.add('btn')
 btn.addEventListener('click', () => {
-    
+    applause.play()
 })
 buttons.appendChild(btn)
