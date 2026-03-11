d0c34027a0eb57a93738599da6f135502820c562
alexanderminton001 <alexanderminton131@gmail.com>
2026-03-11 16:41:46 +0000

    Wed, Mar 11, 2026, 9:41 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -6,6 +6,19 @@ tada = document.querySelector('.tada')
 victory = document.querySelector('.victory')
 wrong = document.querySelector('.wrong')
 
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+
+for (let i = 0; i < 6; i = i + 1) {
+    btn = document.createElement('button')
+    btn.innerText = sounds[i]
+    btn.classList.add('btn')
+    btn.addEventListener('click', () => {
+        applause.play()
+})
+buttons.appendChild(btn)
+}
+
 btn = document.createElement('button')
 btn.innerText = "applause"
 btn.classList.add('btn')
