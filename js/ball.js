26183c2408c32f2bf82ce7717989acf524e1d017
alexanderminton001 <alexanderminton131@gmail.com>
2026-04-13 16:03:55 +0000

    Mon, Apr 13, 2026, 9:03 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,10 +1,13 @@
 const ball = document.createElement('div')
+const ballRadius = 30
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
 
 createBall()
 function createBall(){
     document.body.appendChild(ball)
-    ball.style.height = "100px"
-    ball.style.width = "100px"
+    ball.style.height = `${2 * ballRadius}px`
+    ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
