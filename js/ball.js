d91129002949a7f459a6de793d20438edb2b0666
alexanderminton001 <alexanderminton131@gmail.com>
2026-04-13 16:28:22 +0000

    Mon, Apr 13, 2026, 9:28 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -3,18 +3,26 @@ const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
 let ballXPosition = windowWidth/2 - ballRadius
+let ballYPosition = windowHeight/2 - ballRadius
 let ballSpeed = 5
 let ballXDirection = 1
+let ballYDirection = 1
 
 setInterval(moveBall, 10)
 
 function moveBall(){
     ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
     ball.style.left = `${ballXPosition}px`
+    ball.style.top = `${ballYPosition}px`}
+    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius){
+        ballYDirection = ballYDirection * -1
+    }
     if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius){
         ballXDirection = ballXDirection * -1
     }
 }
+
 
 
 
@@ -26,6 +34,6 @@ function createBall(){
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.top = `${ballYPosition}px`
     ball.style.left = `${ballXPosition}px`
 }
